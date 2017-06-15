716440a0160d75ff4d6243c2b4522b5831384384
Akhil <Akhi1@users.noreply.github.com>
2017-06-15 23:14:32 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  /* scroll to div on nav element select */
+	$("#men1").click(function(){
+	$('html, body').animate({
+	    'scrollTop': $("header").offset().top},
+	    'slow');
+	});
+	$("#men2").click(function(){
+	$('html, body').animate({
+	    'scrollTop': $(".about-me").offset().top},
+	    'slow');
+	});
+	$("#men3").click(function(){
+	$('html, body').animate({
+	    'scrollTop': $(".my-skills").offset().top},
+	    'slow');
+	});
+	$("#men4").click(function(){
+	$('html, body').animate({
+	    'scrollTop': $(".my-projects").offset().top},
+	    'slow');
+	});
+	$("#men5").click(function(){
+	$('html, body').animate({
+	    'scrollTop': $(".contact-me").offset().top},
+	    'slow');
+	});
+
+	/* fix navbar after scroll */
+	$(window).bind('scroll', function(){
+    	if ($(window).scrollTop() > 450){
+        	$('nav').addClass('fixed-nav'); /* you can also use toggleClass() */
+    	}
+    	else{
+        	$('nav').removeClass('fixed-nav');
+    	}
+	});
+});
