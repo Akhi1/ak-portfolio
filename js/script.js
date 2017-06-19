45c84a4ca157de78c6aec04dd0d1f1e354df1249
Akhil <Akhi1@users.noreply.github.com>
2017-06-19 14:30:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,27 +2,28 @@ $(document).ready(function(){
 
   /* scroll to div on nav element select */
 	$("#men1").click(function(){
-	$('html, body').animate({
+		$('html, body').animate({
 	    'scrollTop': $("header").offset().top},
 	    'slow');
+			$(this).addClass(".active");
 	});
 	$("#men2").click(function(){
-	$('html, body').animate({
+		$('html, body').animate({
 	    'scrollTop': $(".about-me").offset().top},
 	    'slow');
 	});
 	$("#men3").click(function(){
-	$('html, body').animate({
+		$('html, body').animate({
 	    'scrollTop': $(".my-skills").offset().top},
 	    'slow');
 	});
 	$("#men4").click(function(){
-	$('html, body').animate({
+		$('html, body').animate({
 	    'scrollTop': $(".my-projects").offset().top},
 	    'slow');
 	});
 	$("#men5").click(function(){
-	$('html, body').animate({
+		$('html, body').animate({
 	    'scrollTop': $(".contact-me").offset().top},
 	    'slow');
 	});
@@ -36,7 +37,9 @@ $(document).ready(function(){
         	$('nav').removeClass('fixed-nav');
     	}
 	});
-
+	/* changing properties of active menu item */
+	
+	
 	/* profile image toggle */
 	$("#profile-img").click(function(){
    		$(this).attr('src', "imgs/profile-pik1.jpg");
